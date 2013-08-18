1344ba7ffb9970b8163712ad6a0238700bba16d7
Olivier Refalo <orefalo@yahoo.com>
2013-08-18 09:15:31 -0400

    clean up repository

--- typescript_tests.js
+++
@@ -1,6 +0,0 @@
-Meteor.__TYPESCRIPT_PRESENT = true;
-
-Tinytest.add("typescript - compile", function (test) {
-    test.isTrue(true);
-});
-//@ sourceMappingURL=typescript_tests.js.map
--- typescript_tests.ts
+++
@@ -1,3 +0,0 @@
-Meteor.__TYPESCRIPT_PRESENT = true;
-
-Tinytest.add("typescript - compile", function (test) { test.isTrue(true); });
