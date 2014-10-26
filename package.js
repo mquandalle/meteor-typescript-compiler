22b24af8da0a1f37530c74a22a68f7c169d18d9c
Olivier Refalo <orefalo@yahoo.com>
2014-10-26 10:02:32 -0400

    be consistent with versioning

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "meteortypescript:compiler",
   summary: "TypeScript is a typed superset of JavaScript",
-  version: "0.0.12",
+  version: "1.0.2",
   git: "https://github.com/meteor-typescript/meteor-typescript-compiler.git"
 });
 
