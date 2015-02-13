1de3f58b5ba155b202490733cfcfeaee51e1976e
Olivier Refalo <orefalo@yahoo.com>
2015-02-13 15:04:48 -0500

    update demos

--- samples/helloworld/test.ts
+++ samples/helloworld/test.ts
@@ -1,6 +1,4 @@
 /// <reference path="lib/node.d.ts"/>
-console.log("------------ IT WORKS -----------------");
-console.log("Hello world Meteor+Typescript.");
-console.log("This string should appear on the client (JavaScript Console)");
-console.log("  ...and the Server console!");
-console.log("---------------------------------------");
+console.log("------------ TypeScript INTEGRATION WORKS -----------------");
+console.log("This text is generatede from TypeScript code!");
+console.log("-----------------------------------------------------------");
--- samples/todos/client/todos.ts
+++ samples/todos/client/todos.ts
@@ -338,21 +338,24 @@ Template['tag_filter'].events({
 
 ////////// Tracking selected list in URL //////////
 
-var TodosRouter = Backbone.Router.extend({
-    routes: {
-        ":list_id": "main"
-    },
-    main: function (list_id) {
+class TodosRouter extends Backbone.Router
+{
+    //routes() {
+    //    ":list_id": "main"
+    //}
+
+    main(list_id) {
         var oldList = Session.get("list_id");
         if (oldList !== list_id) {
             Session.set("list_id", list_id);
             Session.set("tag_filter", null);
         }
-    },
-    setList: function (list_id) {
+    }
+
+    setList(list_id) {
         this.navigate(list_id, true);
     }
-});
+}
 
 var Router = new TodosRouter;
 
