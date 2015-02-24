515d8dd90822063c565567eecc7bd30161fc9cb9
Olivier Refalo <orefalo@yahoo.com>
2015-02-24 00:15:39 -0500

    remove todos js files

--- samples/wip/todos/collections.js
+++
@@ -1,5 +0,0 @@
-/// <reference path="lib/meteor.d.ts"/>
-/// <reference path="collections.d.ts"/>
-Todos = new Mongo.Collection("todos");
-Lists = new Mongo.Collection("lists");
-//# sourceMappingURL=collections.js.map
